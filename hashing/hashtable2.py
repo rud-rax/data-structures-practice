f84b4854fc564738c66a91bfbc74e7f409f5b593
RUDRAKSH <rudrakshnaik8990@gmail.com>
2021-02-16 23:31:57 +0530

    Made hashing program more user friendly

--- hashing/hashtable2.py
+++ hashing/hashtable2.py
@@ -1,7 +1,7 @@
 
 class HashTable :
 
-    def __init__(self,length =10 , probing = 1):
+    def __init__(self,length = 10 , probing = 1):
         
         self.length = length
         self.table = [[None,None]  for _ in range(length)]
@@ -20,7 +20,7 @@ class HashTable :
         for phno,_ in self.table :
             if key == phno :
                 copy_exist = True
-                print("DUPLICATE VALUE FOUND ! CANNOT BE INSERTED IN HASH TABLE.")
+                print("DUPLICATE PHONE NUMBER FOUND ! CANNOT BE INSERTED IN HASH TABLE.")
                 break
 
         while True and not copy_exist :
@@ -52,7 +52,7 @@ class HashTable :
             hash = self.hash_function(key,i)
 
         else :
-            print(f"KEY = {self.table[hash][0]} VALUE = {self.table[hash][1]}")
+            print(f"PHONE NUMBER = {self.table[hash][0]} NAME = {self.table[hash][1]}")
             
 
     def display(self):
@@ -77,16 +77,16 @@ if __name__ == '__main__':
         3 : DISPLAY
         0 : EXIT 
          ''')
-        operation = int(input("ENTER THE OPERATION -> "))
+        operation = int(input("ENTER THE OPERATION YOU WANT TO PERFORM ON HASH TABLE -> "))
 
         if operation == 1 :
-            phno,name = input("ENTER THE KEY AND VALUE -> ").rstrip().split()
+            phno,name = input("ENTER THE PHONE NUMBER AND NAME -> ").rstrip().split()
             phno = int(phno)
             table1.insert(phno,name)
             table1.display()
 
         if operation == 2 :
-            phno = int(input("ENTER THE KEY TO FIND -> "))
+            phno = int(input("ENTER THE PHONE NUMBER TO FIND -> "))
             table1.find(phno)
         
         if operation == 3 :
